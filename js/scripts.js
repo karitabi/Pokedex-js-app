78a9d8445c2e4828bc931da309d6b392349d48fa
karitabi <82491605+karitabi@users.noreply.github.com>
2021-05-19 01:19:08 -0700

    Add pokemon list on JS
    Created an array list of Pokemon and add name, height, and type.

--- js/scripts.js
+++ js/scripts.js
@@ -1,4 +1,6 @@
-alert('Hello World!');
 
-let favoriteFood='Buffalo wings';
-document.write(favoriteFood);
+let pokemonList = 
+    [ {number: 1, name: 'Bulbasaur', height:2.4, type: ['grass','Poison']},
+      {numer: 2, name: 'Ivysaur', height:3.3, type: ['grass','Poison']},
+      {number: 3, name: 'Venusaur', height:6.7, type: ['grass','Poison']}];
+console.log(pokemonList);
