6ed16f4cd42935ef6af72624d3db244e62e67e67
karitabi <82491605+karitabi@users.noreply.github.com>
2021-05-30 18:15:52 -0700

    Updated format
    Updated format for easier reference

--- js/scripts.js
+++ js/scripts.js
@@ -1,9 +1,20 @@
 
 let pokemonList = [ 
-      {number: 1, name: 'Bulbasaur', height:2.4, type: ['grass','Poison']},
-      {numer: 2, name: 'Ivysaur', height:3.3, type: ['grass','Poison']},
-      {number: 3, name: 'Venusaur', height:6.7, type: ['grass','Poison']}
-    ];
+      {number: 1, 
+       name: 'Bulbasaur', 
+       height:2.4, 
+       type: ['grass','Poison']},
+      
+      {numer: 2, 
+       name: 'Ivysaur', 
+       height:3.3, 
+       type: ['grass','Poison']},
+
+      {number: 3, 
+      name: 'Venusaur', 
+      height:6.7, 
+      type: ['grass','Poison']}
+];
 
 for (let i= 0; i<pokemonList.length; i++){
       if (pokemonList[i].height<3){
