dac1f3dad814e50ada13c2aebbd075bcc328771c
karitabi <82491605+karitabi@users.noreply.github.com>
2021-06-15 01:08:53 -0700

    Pushed item
    Pushed one of pokemon off and added IIFE to avoid side effect

--- js/scripts.js
+++ js/scripts.js
@@ -1,5 +1,6 @@
+let pokemonRepository = (function (){
 
-let pokemonList = [ 
+  pokemonList = [ 
       {number: 1, 
        name: 'Bulbasaur', 
        height:2.4, 
@@ -16,7 +17,24 @@ let pokemonList = [
       type: ['grass','Poison']}
 ];
 
-for (let i= 0; i<pokemonList.length; i++){
+  function add(pokemon){
+      pokemonList.push(pokemon);
+    }
+	function getAll(){
+      return pokemonList;
+	  }
+
+   return{
+   getAll: getAll,
+   add: add
+   }
+  })();
+
+  console.log(pokemonRepository.getAll());
+  pokemonRepository.add({number:'4', name:'Charmander', height:'2', type:'lizard'});
+  console.log(pokemonRepository.getAll());
+
+/* for (let i= 0; i<pokemonList.length; i++){
       if (pokemonList[i].height<3){
       document.write("<p>" + pokemonList[i].name+" - " + "Height: " + pokemonList[i].height + " - This is a tiny pokemon!")
     }	else if(pokemonList[i].height>=3 && pokemonList[i].height <5){
@@ -25,5 +43,9 @@ for (let i= 0; i<pokemonList.length; i++){
       document.write("<p>" + pokemonList[i].name+" - " + "Height: " + pokemonList[i].height + " - Wow! This is a big pokemon!")
     } else {document.write("<p>" + pokemonList[i].name+" - " + "Height: " + pokemonList[i].height + " - This is a Giant pokemon!")}
 }
-     
+
+console.log (pokemonRepository.getAll))
+pokemonList.forEach(function(userList){
+	console.log(userList)
+}); */ 
     
